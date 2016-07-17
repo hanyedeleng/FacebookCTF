a0b90f4b558c439cc4377cc9d148cfdd058b5147
hanyedeleng <hanyedeleng@gmail.com>
2016-07-17 10:54:13 -0400

    update vim commands

--- vim.c
+++ vim.c
@@ -31,4 +31,24 @@ Repalce:
 :s/Bill/Steve/      replace the first occurence of Bill by Steve in current line
 :%s/\r//g       delete DOS carriage returns (^M)
 
+Case:
+Vu      lowercase line
+VU      uppercase line
+g~~     Invert case
+vEU     switch word to uppercase
+vE~     Modify word case
+ggguG   Set all text to lowercase
+:set ignorecaes     Ignore case in searches
+:set smartcase      Ignore case in searches excepted if an uppercase letter is used
+:%s/\<./\u&/g       sets first letter of each word to uppercase
+:%s/\<./\|&/g       set first letter of each word to lowcase
+:%s/.*/\u&          set first letter of each line to uppercase
+:%s/.*/\|&          set first letter of each line to lowercase
+
+Read/writefiles
+:1,10 w outfile     save lines 1 to 10 in outfile
+:1,10 w >>outfile   Appends lines 1 to 10 to outfile
+:r infile           Insert the content of infile
+:23r infile         Insert the content of infile under line 23
+
 

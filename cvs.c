de4c6c5cc398059f247d884418ca7176bfb7a3c1
hanyedeleng <hanyedeleng@gmail.com>
2016-07-18 00:06:45 -0400

    add cvs commands

---
+++ cvs.c
@@ -0,0 +1,23 @@
+CVS command:
+Structure           overall structure of CVS commands
+Global options      options you give to the left of cvs_command
+Common options      options you give to the right of cvs_command
+
+
+cvs add         adds a new file/directory to the repository
+cvs admin       administration front end for rcs
+cvs checkout    checkout sources for editions
+cvs commit      checks files into the repository
+cvs diff        runs diffs between revisons
+cvs export      export sources from CVS, similar to checkout
+cvs import      import sources into CVS, using vendor branches
+cvs log         prints out 'rlog' information for files
+cvs rdiff       'path' format diffs between releases
+cvs release     indicate that a Module is no longer in use
+cvs remove      removes an entry from the repository
+cvs rtag        add a tag to a module
+cvs status      status info on the revisions
+cvs tag         add a tag to checked out version
+cvs update      brings work tree in sync with repository
+
+

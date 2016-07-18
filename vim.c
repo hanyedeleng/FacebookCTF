36ef66d3b302f2e807f1965e6d145a0cde4712f7
hanyedeleng <hanyedeleng@gmail.com>
2016-07-17 20:27:24 -0400

    update vim commands 2

--- vim.c
+++ vim.c
@@ -52,3 +52,82 @@ Read/writefiles
 :23r infile         Insert the content of infile under line 23
 
 
+FILE EXPLORER:
+:e .        Open integrated file explorer
+:Sex        Split window and open integrated file explorer
+:browse e   Graphical file explorer
+:ls         List buffers
+:cd ..      Move to parent directory
+:args       List files
+:args *.php Open file list
+grep expression *.php   Returns a list of .php  files contening expression
+gf          Open file name under cursor
+
+INTERACT WITH UNIX
+:!pwd       Execute the pwd unix command, then returns to Vi
+!!pwd       Execute the pwd unix command and insert output in file
+:sh         Temporary returns to Unix
+$exit       Returns to Vi
+
+ALIGNMENT
+:%!fmt      Align all lines
+!}fmt       Align all lines at the current position
+5!!fmt      Align the next 5 lines
+
+
+TABS
+:tabnew     Creates a new tab
+gt          Show next tab
+:tabfirst   Show first tab
+:tabm n(position)   Rearrange tabs
+:tabdo %s/foo/bar/g Execute a command in all tabs
+:tab ball   Puts all open files in tabs
+
+
+WINDOWS SPLITING
+:e filename     Edit filename in current window
+:split filename Split the window and open filename
+ctrl-w up arrow     Puts cursor in top window
+ctrl-w  ctrl-w      Puts cursor in next window
+ctrl-w_             Maximise current window
+ctrl-w=             Gives the same size to all windows
+10 ctrl-w+          Add 10 lines to current window
+:vsplit file        SPlit window vertically
+:sview file         Same as :split in readonly mode
+:hide               Close current window
+:nly                Close all windows, excepted current
+:b 2                Open #2 in this window
+
+
+AUTO-COMPLETION
+Ctrl+n Ctrl+p (in insert mode)      Complete word
+Ctrl+x Ctrl+l                       Complete line
+:set dictionary=dict                Define dict as a dictionnary
+Ctrl+x Ctrl+k                       Complete with dictionary
+
+
+MARKS
+mk      Marks current position as k
+~k      Moves cursor to mark k
+d~k     Delete all until mark k
+
+
+Abbreviations:
+:ab mail mail@provider.org      Define mail as abbreviation of mail@provider.org
+
+
+Text indent:
+:set autoindent     Turn on auto-indent
+:set smartindent    Turn on intelligent auto-indent
+:set shiftwidth=4   Defines 4 spaces as indent size
+ctrl-t, ctrl-d      Indent/un-indent in insert mode
+>>                  Indent
+<<                  un-indent
+
+
+Syntax highlighting:
+:syntax on          Turn on syntax highlighting
+:syntax off         Turn off syntax highlighting
+:set syntax=perl    Force syntax highlighting
+
+

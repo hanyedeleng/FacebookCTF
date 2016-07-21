de87af8073aab11b918d7d1429ef9cc69fe2dd62
hanyedeleng <hanyedeleng@gmail.com>
2016-07-21 06:45:55 -0400

    update gdb display...

--- gdb.c
+++ gdb.c
@@ -61,5 +61,72 @@ info match
 clear                           delete breakpoints at next instruction
 clear [file:]fun
 clear [file:]line
-delete [n]
+delete [n]                      delete breakpoints [or breakpoint n]
+disable [n]                     disable breakpoints [or breakpoint n]
+enable [n]
+enable once [n]
+enable del [n]
+ignore n count                  ignore breakpoint n, count times
+commands n                      execute GDB command-list every time breakpoint n is reached. [silent supresses      [silent]                    default display ]
+end                             end of command-list
 
+Program stack
+backtrace [n]                   print trace of all frames in stack; or of n frames n>0 inner most, n<0, outer
+frame [n]                       select frame number n or frame at address n; if no n, display current frame
+up n                            select frame n frames up
+down n
+info frame [addr]               describe selected frame, or frame at addr
+info args                       arguments of selected frame
+info locals                     local variables of selected frame
+info reg [rn]                   register values [for regs rn] in selected
+info all-reg [rn]               frame; all-reg includes floating point
+
+
+Execution Control
+continue [count]
+c [count]
+step
+s
+
+stepi                           step ny machine instructions rather than source lines
+si
+
+next
+n
+nexti
+ni
+
+until [location]                run until next instruction (or location)
+finish                          run until selected stack frame returns
+return [expr]                   pop selected stack frame without executing [setting return value]
+signal num                      resume execution with signal s (none if 0)
+jump line                       resume execution at spedified line number
+jump *address                   or address
+set var=expr                    evaluate expr without displaying it; use for altering program variables
+
+Display
+print [/f] [expr]               show value of expr [or last value $] according to format f;
+p [/f] [expr]
+    x                           hexadecimal
+    d                           signed decimal
+    u                           unsigned decimal
+    o                           octal
+    t                           binary
+    a                           address, absolute and relative
+    c                           character
+    f                           floating point
+call [/f] expr                  like print but does not display void
+x [/Nuf] expr                   examine memory at address expr; optional format spec follows slash
+  N                             count of how many units to display
+  u                             unit size; one of 
+                                    b individual bytes
+                                    h halfwords (two bytes)
+                                    w words (four bytes)
+                                    g giant words (eight bytes)
+ f                              printing format. Any print format or
+                                    s null-terminated string
+                                    i machine instructions
+disassem [addr]                 display memory as machine instructions
+
+
+            
